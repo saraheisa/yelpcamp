f58847d3f4082494cea7309a487e05b321780bbc
Sara Eisa <prosarah10@gmail.com>
2020-06-05 23:02:55 +0200

    add price to campground model

--- models/campground.js
+++ models/campground.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const campgroundSchema = new mongoose.Schema({
     name: String,
+    price: Number,
     image: String,
     description: String,
     comments: [
