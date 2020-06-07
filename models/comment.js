c9b1ef97dd583df0b6bfff5b9a1db9baf42a7e07
Sara Eisa <prosarah10@gmail.com>
2020-06-07 17:27:12 +0200

    add vote to comment model

--- models/comment.js
+++ models/comment.js
@@ -8,7 +8,8 @@ const commentSchema = new mongoose.Schema({
             ref: 'User'
         },
         username: String
-    }
+    },
+    vote: Number
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
